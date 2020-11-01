63ae129119c67d9b3e529d8b72d03e0ec5aa4830
unknown <ertebateweb@gmail.com>
2020-11-01 19:28:44 +0330

    Edit signal views.py home to show Signals

--- Signals/views.py
+++ Signals/views.py
@@ -8,6 +8,8 @@ def home(request):
     qs = signal.objects.all()
     Signals_title = list()
     for s in qs:
-        Signals_title.append(s.SymbolTitle)
-    response_html = '<br>'.join(Signals_title)
+        Signals_title.append("symbol:{}--Now Price:{}--Active:{}--created_at:{}--created_by{}".format(s.SymbolTitle,s.NowPrice,s.IsActive,s.created_at,s.created_by))
+    
+    response_html = Signals_title
+    
     return HttpResponse(response_html)
