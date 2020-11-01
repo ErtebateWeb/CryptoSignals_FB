50e8316e6a203ac6ecc23e4a9e734ab97e088077
unknown <ertebateweb@gmail.com>
2020-11-01 19:07:28 +0330

    Add signal views to show signals list

--- Signals/views.py
+++ Signals/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import signal
+ 
 # Create your views here.
+
+def home(request):
+    qs = signal.objects.all()
+    Signals_title = list()
+    for s in qs:
+        Signals_title.append(s.SymbolTitle)
+    response_html = '<br>'.join(Signals_title)
+    return HttpResponse(response_html)
