dc863be78e44c93849ddf1f545bac1376d80a92b
unknown <ertebateweb@gmail.com>
2020-11-02 23:25:36 +0330

    Add signals /index  views

--- Signals/views.py
+++ Signals/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from .models import signal
  
 # Create your views here.
+def index(request):
+    context ={}
+    return render(request,'signals/index.html',context)
+
+
+
 
 def home(request):
     qs = signal.objects.all()
@@ -11,3 +17,15 @@ def home(request):
     }
     print(context)
     return render(request, "signals/signal_list.html", context)
+
+
+
+def submit_signal(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signals/signal_list.html')
+    else:
+        form = PostForm()
+    return render(request, 'submit_signal.html', {'form': form})
