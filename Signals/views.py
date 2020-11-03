51b09ed91173d6fdf1c92fe81c45e6810295fa7e
unknown <ertebateweb@gmail.com>
2020-11-03 09:06:09 +0330

    add submit_signal

--- Signals/views.py
+++ Signals/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import signal
+from .forms import submitsignalForm
  
 # Create your views here.
-def index(request):
-    signal_list = signal.objects.all().filter(IsActive=True).order_by('-id')
-    context ={'signal_list': signal_list}
-    return render(request,'signals/index.html',context)
+# def index(request):
+#     signal_list = signal.objects.all().filter(IsActive=True).order_by('-id')
+#     context ={'signal_list': signal_list}
+#     return render(request,'signals/index.html',context)
 
 
 
@@ -21,12 +22,27 @@ def home(request):
 
 
 
-# def submit_signal(request):
-#     if request.method == 'POST':
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('signals/signal_list.html')
-#     else:
-#         form = PostForm()
-#     return render(request, 'submit_signal.html', {'form': form})
+def submit_signal(request):
+    # if request.method == 'POST':
+    #     form = PostForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('signals/signal_list.html')
+    # else:
+    #     form = PostForm()
+    # return render(request, 'submit_signal.html', {'form': form})
+    signal_list = signal.objects.all().filter(IsActive=True).order_by('-id')
+    form = submitsignalForm()
+    context ={'signal_list': signal_list,'frm': form}
+    
+    
+    return render(request,'signals/index.html',context)
+
+
+def addsignal(request):
+    form = submitsignalForm(request.POST)
+    print(request.POST['SymbolTitle'])
+    return redirect('submit')
+
+
+
