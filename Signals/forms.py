fc28e37a0c9c11406355a92595ff21f12001ff5d
unknown <ertebateweb@gmail.com>
2020-11-05 13:31:14 +0330

    add signalModelForm

--- Signals/forms.py
+++ Signals/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.forms import ModelForm
+from .models import signal
 
 class submitsignalForm(forms.Form):
     SymbolTitle = forms.CharField(max_length=40, 
@@ -29,3 +30,10 @@ class submitsignalForm(forms.Form):
     TakeProfit4 = forms.CharField(max_length=40, 
                 widget=forms.TextInput(
                     attrs={'class' : 'form-control', 'placeholder' : 'Enter TakeProfit4', 'aria-label' : 'TakeProfit4', 'aria-describedby' : 'add-btn'}))
+
+
+class SignalModelForm(ModelForm):
+    class Meta:
+        model = signal
+        # fields = '__all__'
+        fields =['SymbolTitle','TimeFrame','NowPrice','TriggerPrice','StopLoss','TakeProfit1','TakeProfit2','TakeProfit3','TakeProfit4',]
